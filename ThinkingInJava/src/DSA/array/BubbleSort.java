d029796a737b2667971dbbb9372926b514c1499f
yhc <1298394723@qq.com>
2023-11-30 17:08:06 +0800

    冒泡排序

--- ThinkingInJava/src/DSA/array/BubbleSort.java
+++ ThinkingInJava/src/DSA/array/BubbleSort.java
@@ -1,54 +1,43 @@
 package DSA.array;
+
+/**
+ * 冒泡排序
+ */
 public class BubbleSort implements Sort{
-    private int exchangeNum = 0;
-    private int CompareNum = 0;
-    public void lowSort(int list[]) {
-        int lo = 0,hi = list.length;
+
+    @Override
+    public void sort(int[] array) {
+        int lo = 0,hi = array.length;
         while(lo < hi--){
-            for(int i=0;i<hi-1;i++){
-                if(list[i]>list[i+1]){
-                    exchange(list,i,i+1);
-                    exchangeNum++;
+            for(int i=0; i<hi-1; i++){
+                if(array[i] > array[i+1]){
+                    exchange(array,i,i+1);
                 }
-                CompareNum++;
             }
         }
     }
 
-    @Override
-    public void sort(int list[]) {
-        int lo = 0,hi = list.length;
+    public void lowSort(int[] array) {
+        int lo = 0,hi = array.length;
         int n = hi;
         boolean flag = true;
-        while(lo < hi&&flag){
+        while(lo < hi && flag){
             flag = false;
-            for(int i=0;i<hi-1;i++){
-                if(list[i]>list[i+1]){
-                    exchange(list,i,i+1);
-                    exchangeNum++;
+            for(int i=0; i<hi-1; i++){
+                if(array[i] > array[i+1]){
+                    exchange(array,i,i+1);
                     n = i+1;
                     flag = true;
                 }
-                CompareNum++;
             }
             hi = n;
         }
     }
 
-    public void test(int n) {
-        int [] array = getRandomList(n);
-        //show(array);
-        long startTime = System.currentTimeMillis();
-        lowSort(array);
-        long endTime = System.currentTimeMillis();
-        //show(array);
-        System.out.println("比较次数："+CompareNum);
-        System.out.println("交换次数："+exchangeNum);
-        System.out.println("程序运行时间：" + (endTime - startTime) + "ms");
-    }
-
     public static void main(String[] args) {
-        Sort test = new BubbleSort();
-        test.test(100000);
+        Sort bubble = new BubbleSort();
+        TestUtil.test(1000,bubble);
+        TestUtil.test(10000,bubble);
+        TestUtil.test(100000,bubble);
     }
 }
--- ThinkingInJava/src/DSA/array/Sort.java
+++ ThinkingInJava/src/DSA/array/Sort.java
@@ -1,24 +1,12 @@
 package DSA.array;
 
-import java.util.Arrays;
-import java.util.Random;
-
 public interface Sort {
-    void sort(int list[]);
-    void test(int n);
 
-    default void exchange(int list[],int i,int j){
-        int item = list[i];
-        list[i] = list[j];
-        list[j] = item;
-    }
+    void sort(int[] array);
 
-    default int[] getRandomList(int n){
-        int [] array = new Random().ints(n,1,n).toArray();
-        return array;
-    }
-
-    default void show(int array[]){
-        System.out.println(Arrays.toString(array));
+    default void exchange(int[] array, int i, int j){
+        int item = array[i];
+        array[i] = array[j];
+        array[j] = item;
     }
 }
