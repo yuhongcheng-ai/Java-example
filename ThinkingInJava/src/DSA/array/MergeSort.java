0c6a5ce44f11c63f76eeed69b7a13e0092800789
yhc <1298394723@qq.com>
2023-12-12 10:55:41 +0800

    归并排序

--- ThinkingInJava/src/DSA/array/MergeSort.java
+++ ThinkingInJava/src/DSA/array/MergeSort.java
@@ -1,77 +1,88 @@
 package DSA.array;
 
-import java.util.Arrays;
-
+/**
+ * 归并排序
+ */
 public class MergeSort implements Sort{
-    private int exchangeNum = 0;
-    private int CompareNum = 0;
+    private int[] tem;
     @Override
     public void sort(int[] arr) {
-        int len = arr.length;
-        int[] result = new int[len];
-        int block, start;
-
-        for(block = 1; block < len*2; block *= 2) {
-            for(start = 0; start <len; start += 2 * block) {
-                int low = start;
-                int mid = (start + block) < len ? (start + block) : len;
-                int high = (start + 2 * block) < len ? (start + 2 * block) : len;
-                //两个块的起始下标及结束下标
-                int start1 = low, end1 = mid;
-                int start2 = mid, end2 = high;
-                //开始对两个block进行归并排序
-                while (start1 < end1 && start2 < end2) {
-                    result[low++] = arr[start1] < arr[start2] ? arr[start1++] : arr[start2++];
-                }
-                while(start1 < end1) {
-                    result[low++] = arr[start1++];
-                }
-                while(start2 < end2) {
-                    result[low++] = arr[start2++];
-                }
-            }
-            int[] temp = arr;
-            arr = result;
-            result = temp;
-        }
-        result = arr;
+        tem = new int[arr.length];
+        merge_sort(arr,0,arr.length);
+        //merge_sort(arr);
     }
 
-    @Override
-    public void test(int n) {
-        int [] array = getRandomList(n);
-       // show(array);
-        long startTime = System.currentTimeMillis();
-        sort(array);//迭代
-        //merge_sort_recursive(array,new int[array.length],0,array.length-1);//递归
-        long endTime = System.currentTimeMillis();
-        //show(array);
-        System.out.println("程序运行时间：" + (endTime - startTime) + "ms");
-    }
-
-    public void merge_sort_recursive(int[] arr, int[] result, int start, int end) {
-        if(start >= end){
+    /**
+     * 自顶向下的递归式归并排序
+     */
+    public void merge_sort(int[] arr, int lo, int hi) {
+        if(hi - lo < 15){
+            insertionSort(arr, lo, hi);
             return;
         }
-        int mid = (start + end)>>1;
-        int start1 = start, end1 = mid;
-        int start2 = mid + 1, end2 = end;
-        merge_sort_recursive(arr, result, start1, end1);
-        merge_sort_recursive(arr, result, start2, end2);
-        int k = start;
-        while(start1<=end1&&start2<=end2)
-            result[k++] = arr[start1] < arr[start2] ? arr[start1++] : arr[start2++];
-        while(start1 <= end1)
-            result[k++] = arr[start1++];
-        while(start2 <= end2)
-            result[k++] = arr[start2++];
-        for(k=start;k<=end;k++){
-            arr[k] = result[k];
+        int mid = (lo + hi)>>1;
+        merge_sort(arr, lo, mid);
+        merge_sort(arr, mid, hi);
+        merge(arr,lo,mid,hi);
+    }
+
+    /**
+     * 插入排序
+     */
+    private void insertionSort(int[] array, int lo, int hi){
+        int lef = lo;
+        lo++;
+        while(lo < hi){
+            for(int i=lo;i>lef;i--){
+                if(array[i] < array[i-1]){
+                    exchange(array,i,i-1);
+                }
+            }
+            lo++;
+        }
+    }
+
+    /**
+     * 自底向上的归并排序
+     */
+    public void merge_sort(int[] arr){
+        int length = arr.length;
+        int len = 1;//子数组的大小
+        while (len < length){
+            for (int lo = 0; lo < length-len; lo += 2*len) {
+                merge(arr, lo, lo+len, Math.min(lo+2*len, length));
+            }
+            len = 2*len;
+        }
+    }
+
+    /**
+     * 二路归并
+     */
+    public void merge(int[] arr, int lo, int mid, int hi){
+        if(hi - lo == 1) return;
+        int k = lo;
+        while (k < mid){
+            tem[k] = arr[k++];//备份左子数组，右子数组在结果数组中，归并的元素先放在结果数组的左边，所以右子数组不会被覆盖
+        }
+
+        int i = lo;//左子数组的指针
+        int j = mid;//右子数组的指针
+        int p = lo;//结果数组的指针
+        while (p < hi){//p移动到hi时说明归并完成
+            if(i == mid)
+                break;//左子数组元素耗完直接退出
+            else if(j == hi)
+                arr[p++] = tem[i++];
+            else if(tem[i] <= arr[j])
+                arr[p++] = tem[i++];
+            else
+                arr[p++] = arr[j++];
         }
     }
 
     public static void main(String[] args) {
-        Sort test = new MergeSort();
-        test.test(200000000);//程序运行时间：2463ms
+        MergeSort mergeSort = new MergeSort();
+        TestUtil.test(10000,mergeSort);
     }
 }
